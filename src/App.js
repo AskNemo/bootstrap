221caa628ab9631af24977ae5c8b22127d610c5e
Anoop Varghese <anupvarghese@gmail.com>
2017-03-31 16:06:55 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h2>Welcome to React</h2>
+        </header>
+        <aside>Side</aside>
+        <section>
+          section
+        </section>
+        <footer>footer</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
